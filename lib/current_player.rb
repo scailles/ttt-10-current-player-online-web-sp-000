f4f26c5ddc3bcfc90960e2425a58a8929e5d1ed5
Shaina C <scailles23@gmail.com>
2020-11-18 06:15:16 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,7 +3,7 @@
 def turn_count(board)
   turn_num = 0
   board.each do |space|
-    if space == "X" || space == "O"
+    if space == "X" || space == "0"
       turn_num +=1
     end
     return turn_num
